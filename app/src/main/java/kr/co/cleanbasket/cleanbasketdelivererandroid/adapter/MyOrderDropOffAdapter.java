4438a89baa196f21394abca9b45f0a1ce0de3e77
GingerAebi <gingerAebi@gmail.com>
2016-06-29 17:19:05 +0900

    gray background change

--- app/src/main/java/kr/co/cleanbasket/cleanbasketdelivererandroid/adapter/MyOrderDropOffAdapter.java
+++ app/src/main/java/kr/co/cleanbasket/cleanbasketdelivererandroid/adapter/MyOrderDropOffAdapter.java
@@ -1,20 +1,12 @@
 package kr.co.cleanbasket.cleanbasketdelivererandroid.adapter;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -23,12 +15,7 @@ import java.util.ArrayList;
 
 import kr.co.cleanbasket.cleanbasketdelivererandroid.R;
 import kr.co.cleanbasket.cleanbasketdelivererandroid.network.OrderManager;
-import kr.co.cleanbasket.cleanbasketdelivererandroid.vo.JsonData;
 import kr.co.cleanbasket.cleanbasketdelivererandroid.vo.OrderInfo;
-import kr.co.cleanbasket.cleanbasketdelivererandroid.vo.OrderRequest;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * MyOrderDropOffAdapter.java
@@ -81,6 +68,13 @@ public class MyOrderDropOffAdapter extends BaseAdapter {
         TextView item = (TextView) row.findViewById(R.id.item);
         TextView memo = (TextView) row.findViewById(R.id.memo);
 
+        TextView t1 = (TextView) row.findViewById(R.id.t1);
+        TextView t2 = (TextView) row.findViewById(R.id.t2);
+        TextView t3 = (TextView) row.findViewById(R.id.t3);
+        TextView t4 = (TextView) row.findViewById(R.id.t4);
+        TextView t5 = (TextView) row.findViewById(R.id.t5);
+        TextView t6 = (TextView) row.findViewById(R.id.t6);
+
         DecimalFormat df = new DecimalFormat("#,##0");
 
         order_number.setText(orderArrayList.get(position).order_number);
@@ -95,90 +89,22 @@ public class MyOrderDropOffAdapter extends BaseAdapter {
         memo.setText(orderArrayList.get(position).memo);
 
         if (orderArrayList.get(position).state == 4) {
-            row.setBackgroundColor(Color.GRAY);
+            order_number.setTextColor(row.getResources().getColor(R.color.gray));
+            dropoff_date.setTextColor(row.getResources().getColor(R.color.gray));
+            address.setTextColor(row.getResources().getColor(R.color.gray));
+            price.setTextColor(row.getResources().getColor(R.color.gray));
+            memo.setTextColor(row.getResources().getColor(R.color.gray));
+            item.setTextColor(row.getResources().getColor(R.color.gray));
+
+            t1.setTextColor(row.getResources().getColor(R.color.gray));
+            t2.setTextColor(row.getResources().getColor(R.color.gray));
+            t3.setTextColor(row.getResources().getColor(R.color.gray));
+            t4.setTextColor(row.getResources().getColor(R.color.gray));
+            t5.setTextColor(row.getResources().getColor(R.color.gray));
+            t6.setTextColor(row.getResources().getColor(R.color.gray));
         }
 
         return row;
-    }
-
-    public void showOrderDetailDialog(final int position) {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        // Get the layout inflater
-        LayoutInflater inflater = context.getLayoutInflater();
-
-        final View view = inflater.inflate(R.layout.dialog_order1, null);
-
-        TextView order_number = (TextView) view.findViewById(R.id.order_number);
-        TextView price = (TextView) view.findViewById(R.id.price);
-        TextView pickup_date = (TextView) view.findViewById(R.id.pickup_date);
-        TextView dropoff_date = (TextView) view.findViewById(R.id.dropoff_date);
-        TextView address = (TextView) view.findViewById(R.id.address);
-        TextView item = (TextView) view.findViewById(R.id.itemCodes);
-        TextView memo = (TextView) view.findViewById(R.id.memo);
-        TextView phone = (TextView) view.findViewById(R.id.phone);
-
-        final EditText note = (EditText) view.findViewById(R.id.note);
-
-        order_number.setText(orderArrayList.get(position).order_number);
-
-        DecimalFormat df = new DecimalFormat("#,##0");
-
-        order_number.setText(orderArrayList.get(position).order_number);
-
-        String price_str = "";
-
-        if (orderArrayList.get(position).coupon.isEmpty()) {
-            price_str = String.valueOf(df.format(orderArrayList.get(position).price) + " (" + orderArrayList.get(position).getPriceStatus() + ")");
-        } else {
-            price_str = String.valueOf(df.format(orderArrayList.get(position).price)) + " (" + orderArrayList.get(position).getPriceStatus() + "/" + orderArrayList.get(position).coupon.get(position).name + " )";
-        }
-        price.setText(price_str);
-
-        pickup_date.setText(orderArrayList.get(position).pickup_date);
-        dropoff_date.setText(orderArrayList.get(position).dropoff_date);
-        address.setText(orderArrayList.get(position).getFullAddress());
-        item.setText(orderArrayList.get(position).makeItem());
-        memo.setText(orderArrayList.get(position).memo);
-        phone.setText(orderArrayList.get(position).phone);
-
-        if (!orderArrayList.get(position).note.equals("")) {
-            note.setText(orderArrayList.get(position).note);
-        }
-
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
-        builder.setView(view)
-                // Add action buttons
-                .setPositiveButton("배달완료", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        String value = note.getText().toString();
-                        orderManager.sendDropOffComplete(new OrderRequest("" + orderArrayList.get(position).oid, value), new Callback<JsonData>() {
-                            @Override
-                            public void onResponse(Call<JsonData> call, Response<JsonData> response) {
-                                JsonData jsonData = response.body();
-                                myOrderDropOffAdapter.notifyDataSetChanged();
-                            }
-
-                            @Override
-                            public void onFailure(Call<JsonData> call, Throwable t) {
-
-                            }
-                        });
-                    }
-                })
-                .setNegativeButton("번호 복사", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        ClipboardManager clipboardManage = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-                        ClipData clipData = ClipData.newPlainText(orderArrayList.get(position).phone, orderArrayList.get(position).phone);
-                        clipboardManage.setPrimaryClip(clipData);
-                        Toast.makeText(context, "번호 복사가 완료되었습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-        builder.create();
-        builder.show();
-
     }
 
 }
--- app/src/main/java/kr/co/cleanbasket/cleanbasketdelivererandroid/adapter/MyOrderPickUpAdapter.java
+++ app/src/main/java/kr/co/cleanbasket/cleanbasketdelivererandroid/adapter/MyOrderPickUpAdapter.java
@@ -64,6 +64,14 @@ public class MyOrderPickUpAdapter extends BaseAdapter {
         TextView memo = (TextView) row.findViewById(R.id.memo);
         TextView item = (TextView) row.findViewById(R.id.item);
 
+        TextView t1 = (TextView) row.findViewById(R.id.t1);
+        TextView t2 = (TextView) row.findViewById(R.id.t2);
+        TextView t3 = (TextView) row.findViewById(R.id.t3);
+        TextView t4 = (TextView) row.findViewById(R.id.t4);
+        TextView t5 = (TextView) row.findViewById(R.id.t5);
+        TextView t6 = (TextView) row.findViewById(R.id.t6);
+
+
         order_number.setText(orderArrayList.get(position).order_number);
         pickup_date.setText(orderArrayList.get(position).getPrettyPickUpDate());
         address.setText(orderArrayList.get(position).getFullAddress());
@@ -79,7 +87,19 @@ public class MyOrderPickUpAdapter extends BaseAdapter {
         // 완료 건에 대한 색 변경
 
         if (orderArrayList.get(position).state >= 2) {
-            row.setBackgroundColor(Color.GRAY);
+            order_number.setTextColor(row.getResources().getColor(R.color.gray));
+            pickup_date.setTextColor(row.getResources().getColor(R.color.gray));
+            address.setTextColor(row.getResources().getColor(R.color.gray));
+            price.setTextColor(row.getResources().getColor(R.color.gray));
+            memo.setTextColor(row.getResources().getColor(R.color.gray));
+            item.setTextColor(row.getResources().getColor(R.color.gray));
+
+            t1.setTextColor(row.getResources().getColor(R.color.gray));
+            t2.setTextColor(row.getResources().getColor(R.color.gray));
+            t3.setTextColor(row.getResources().getColor(R.color.gray));
+            t4.setTextColor(row.getResources().getColor(R.color.gray));
+            t5.setTextColor(row.getResources().getColor(R.color.gray));
+            t6.setTextColor(row.getResources().getColor(R.color.gray));
         }
 
         return row;
